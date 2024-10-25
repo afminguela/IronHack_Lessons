7477a24b574d69fde74e5e740a830cd23fb2c08b
Ana Fernandez Minguela <afminguela@gmail.com>
2024-10-25 17:58:21 +0200

    Inicio en una línea yuhugit add .git add .! añadida clase inicio

--- UF2404/Java-Exercises/EC_Taller/src/main/java/home/afm/ec_taller/EC_AppTaller.java
+++ UF2404/Java-Exercises/EC_Taller/src/main/java/home/afm/ec_taller/EC_AppTaller.java
@@ -17,41 +17,10 @@ public class EC_AppTaller {
 // _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_ Inicio Programa _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*      
         public static void main(String[] args) {
     
-  
-        Taller taller = new Taller("Talleres Manolo");
-        Scanner scanner = new Scanner(System.in);
-        
-        
-        System.out.println("-+-+-+-+-+-+-+--+ BIENVENIDO A "+ taller.getNombre() +" -+-+-+-+-+-+-+-+-+-");
-        while (true) {
-            System.out.println("\nMenu principal:");
-            System.out.println("1. Menu Clientes");
-            System.out.println("2. Menu Vehículos");
-            System.out.println("3. Menu Reparaciones");
-            System.out.println("4. Salir");
-
-            System.out.print("\n¿Que quieres hacer? : ");
-            int opcionPrincipal = scanner.nextInt();
-            scanner.nextLine(); 
-
-            switch (opcionPrincipal) {
-                case 1:
-                    Controlador.menuClientes(scanner, taller);
-                    break;
-                case 2:
-                    Controlador.menuVehiculos(scanner, taller);
-                    break;
-                case 3:
-                    Controlador.menuReparaciones(scanner, taller);
-                    break;
-                case 4:
-                    System.out.println("Gracias por utilizar el sistema.");
-                    return;
-                default:
-                    System.out.println("Opción inválida. Por favor, intente de nuevo.");
-            }
+            Inicio.iniciarFiesta();
         }
-    }
+}
+    
 
      // *_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*  FIN DEL PROGRAMA _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*    
         
